6c85b1631a0c54f9d40cf7eb69724955774d979f
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-02-11 22:42:06 +0530

    Header section completed

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,59 @@
 import React from "react";
+import { signOut } from "next-auth/react";
+import Button from "@mui/material/Button";
+import MenuIcon from "@mui/icons-material/Menu";
+import Image from "next/image";
+import { IconButton } from "@mui/material";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import SearchIcon from "@mui/icons-material/Search";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AppsIcon from "@mui/icons-material/Apps";
+import { Avatar } from "@mui/material";
 
 function Header() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <header className=" w-screen h-16 flex justify-between items-center p-4 border-b-2">
+        <div className="flex items-center">
+          <div className="flex items-center gap-4">
+            <IconButton aria-label="menu icon">
+              <MenuIcon className="!cursor-pointer" />
+            </IconButton>
+            <Image src="https://rb.gy/gzkehd" width={48} height={48} />
+            <span className="text-2xl text-gray-600">Calendar</span>
+          </div>
+          <div className="flex items-center ml-12">
+            <Button variant="outlined">Today</Button>
+            <IconButton aria-label="Left icon">
+              <ChevronLeftIcon />
+            </IconButton>
+            <IconButton aria-label="Right icon">
+              <ChevronRightIcon />
+            </IconButton>
+            <span className="text-[1.5rem] text-gray-700">Feburuary 2022</span>
+          </div>
+        </div>
+        <div className="flex items-center gap-6">
+          <SearchIcon />
+          <HelpOutlineIcon />
+          <SettingsIcon />
+          <AppsIcon />
+          <IconButton aria-label="Right icon" onClick={signOut}>
+            <Avatar>Ak</Avatar>
+          </IconButton>
+        </div>
+      </header>
+      {/* <Button
+        style={{ backgroundColor: "#1971c2" }}
+        variant="contained"
+        onClick={signOut}
+      >
+        Sign Out
+      </Button> */}
+    </div>
+  );
 }
 
 export default Header;
